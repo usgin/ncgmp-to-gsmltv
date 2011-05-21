4334f4e1754771852576ef6f016278a7278fa9c4
Ryan Clark <ryan.clark.j@gmail.com>
2011-05-20 18:41:49 -0700

    Changed it so that the mapping for GeologicUnitView does not use a the getID() function. This meant changing the view (sql) and the mapping file. This solves some problems in older versions of Geoserver/GeoTools.

--- sql/geologic-unit-view.sql
+++ sql/geologic-unit-view.sql
@@ -1,30 +1,11 @@
-﻿SELECT 	polys.mapunitpolys_id AS featureuri, 
-	dmu.name AS name, 
-	dmu.fullname AS fullname, 
-	dmu.label AS label,
-	'urn:ogc:def:nil:OGC:missing'::character varying AS parenturi,
-	dmu.description AS description,
-	dmu.areafillrgb AS symbol,
-	dmu.descriptionofmapunits_id AS specificationuri,
-	'urn:cgi:classifier:CGI:GeologicUnitType:200811:lithostratigraphic_unit'::character varying AS geologicunittype,
-	preferredage.agedisplay AS geologichistory,
-	CASE preferredage.agedisplayurn IS NULL
-		WHEN true THEN preferredage.youngerurn
-		ELSE preferredage.agedisplayurn
-	END AS representativeageuri,
-	preferredage.youngerurn AS youngerageuri,
-	preferredage.olderurn AS olderageuri,
-	dmu.hierarchykey AS rank,
-	dmuext.lithology_summary AS lithology,
-	ccl.cgiurn AS representativelithologyuri,
-	'urn:ogc:def:nil:OGC:missing'::character varying AS observationmethod,
-	polys.datasourceid AS sourceuri,
-	polys.datasourceid AS metadatauri,
-	polys.shape AS shape
-
-FROM public.ncgmp_mapunitpolys AS polys 
-JOIN public.ncgmp_descriptionofmapunits AS dmu on polys.mapunit = dmu.mapunit
-JOIN public.ncgmp_datasources AS ds on polys.datasourceid = ds.datasources_id
-JOIN usgin.geologicevent AS preferredage on dmu.descriptionofmapunits_id = preferredage.preferredage_dmu_id
-LEFT JOIN public.ncgmp_ex_dmuextension AS dmuext on dmu.mapunit = dmuext.mapunit
-LEFT JOIN usgin.controlledconceptlookup AS ccl on dmuext.representative_lithology = ccl.lookupterm
+SELECT 'geologicunitview.' || row_number() OVER (ORDER BY polys.mapunitpolys_id) AS id, polys.mapunitpolys_id AS featureuri, dmu.name, dmu.fullname, dmu.label, 'urn:ogc:def:nil:OGC:missing'::character varying AS parenturi, dmu.description, dmu.areafillrgb AS symbol, dmu.descriptionofmapunits_id AS specificationuri, 'urn:cgi:classifier:CGI:GeologicUnitType:200811:lithostratigraphic_unit'::character varying AS geologicunittype, preferredage.agedisplay AS geologichistory, 
+        CASE preferredage.agedisplayurn IS NULL
+            WHEN true THEN preferredage.youngerurn
+            ELSE preferredage.agedisplayurn
+        END AS representativeageuri, preferredage.youngerurn AS youngerageuri, preferredage.olderurn AS olderageuri, dmu.hierarchykey AS rank, dmuext.lithology_summary AS lithology, ccl.cgiurn AS representativelithologyuri, 'urn:ogc:def:nil:OGC:missing'::character varying AS observationmethod, polys.datasourceid AS sourceuri, polys.datasourceid AS metadatauri, polys.shape
+   FROM ncgmp_mapunitpolys polys
+   JOIN ncgmp_descriptionofmapunits dmu ON polys.mapunit::text = dmu.mapunit::text
+   JOIN ncgmp_datasources ds ON polys.datasourceid::text = ds.datasources_id::text
+   JOIN usgin.geologicevent preferredage ON dmu.descriptionofmapunits_id::text = preferredage.preferredage_dmu_id::text
+   LEFT JOIN ncgmp_ex_dmuextension dmuext ON dmu.mapunit::text = dmuext.mapunit::text
+   LEFT JOIN usgin.controlledconceptlookup ccl ON dmuext.representative_lithology = ccl.lookupterm::text;
